1a735d209e3b5b2022446c1042fa24d79d5f2ff2
johlbaum <julien.ohlbaum@gmail.com>
2024-10-28 14:41:56 +0100

    Header font size

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const mobileMenu = document.querySelector('.mobile-menu');
   const burgerButton = document.querySelector('.burger');
+  const menuLinks = document.querySelectorAll('.mobile-menu ul li a');
 
+  // Ouvrir/Fermer le menu quand on clique sur le bouton burger
   burgerButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
+    burgerButton.classList.toggle('open');
+  });
+
+  // Fermer le menu mobile quand on clique sur un lien du menu
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('show');
+      burgerButton.classList.remove('open');
+    });
   });
 });
